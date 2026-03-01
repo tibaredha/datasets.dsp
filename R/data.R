8c9f39c9887169834424ad5a0c68fb2a1cb32619
Tibaredha <tibaredha@yahoo.fr>
2026-03-01 16:12:04 +0100

    maj

--- R/data.R
+++ R/data.R
@@ -18,7 +18,7 @@
 "bloc"
 
 
-#' rh
+#' ressource humaine
 #'
 #' Look up rh.
 #'
@@ -38,21 +38,32 @@
 #'   rh
 "rh"
 
-
-#' deceshosp
+#' mortalité hospitalière
 #'
 #' Look up deceshosp.
 #'
 #' @source DSP Djelfa
-#' @format Data frame with 07 columns  and 51,397 rows
+#' @format Data frame with 19 columns  and 20584 rows
 #' \describe{
-#' \item{STRUCTURE}{établissement.}
-#' \item{AGE}{age en année.}
-#' \item{SEXE}{sexe.}
-#' \item{DATE_INTERVENTION}{date intervention.}
-#' \item{MEDECIN}{médecin.}
-#' \item{DIAGNOSTIC}{diagnostic.}
-#' \item{TYPE}{type.}
+#' \item{DINS}{date déces.}
+#' \item{DATENAISSANCE}{date de naissance.}
+#' \item{SEX}{sexe.}
+#' \item{Years}{age en année.}
+#' \item{Days}{age en jours.}
+#' \item{WILAYAR}{wilaya de résidence.}
+#' \item{WILAYAS}{wilaya de naissance.}
+#' \item{COMMUNER}{comuune de résidence.}
+#' \item{COMMUNE}{commune de naissance.}
+#' \item{LD}{lieux du déces.}
+#' \item{structure}{établissement du déces.}
+#' \item{service}{service du déces.}
+#' \item{DUREEHOSPIT}{durée d'hospitalisation.}
+#' \item{n_profession}{la profession.}
+#' \item{CD}{cause du déces.}
+#' \item{CHAP}{chapitre.}
+#' \item{diag_cod}{code cim.}
+#' \item{diag_nom}{dignostic.}
+#' \item{MEDECINHOSPIT}{médecin.}
 #' }
 #' @examples
 #'   deceshosp
